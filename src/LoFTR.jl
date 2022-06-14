b08de40e5a2d888055df3471a6058ce9b90e8d69
Aman <aman.sharma2020b@vitstudent.ac.in>
2022-06-14 18:46:50 +0530

    Additon of formatting file

--- src/LoFTR.jl
+++ src/LoFTR.jl
@@ -1,5 +1,9 @@
 module LoFTR
 
 # Write your package code here.
-
+include("utils/coarse_matching.jl")
+include("utils/fine_matching.jl")
+include("utils/geometry.jl")
+include("utils/position_encoding.jl")
+include("utils/supervision.jl")
 end
