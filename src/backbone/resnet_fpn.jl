aea51b502c2fc4aae53c7eb3c7978d2bf6f05169
Aman <aman.sharma2020b@vitstudent.ac.in>
2022-07-05 16:41:29 +0530

    resnet_fpn added

--- src/backbone/resnet_fpn.jl
+++ src/backbone/resnet_fpn.jl
@@ -8,7 +8,7 @@ function conv3x3(in_planes, out_planes, stride=1)
     # """3x3 convolution without padding."""
 end
 
-function BasicBlock(in_planes, out_planes,stride=1,x)
+function BasicBlock(in_planes, out_planes,stride=1,x::AbstractArray)
   conv1 = conv3x3(in_planes, out_planes, stride)
   conv2 = conv3x3(out_planes,out_planes,stride)
   bn1 = BatchNorm(out_planes)
@@ -30,31 +30,32 @@ function BasicBlock(in_planes, out_planes,stride=1,x)
   return relu(x+y)
 end
 
-function _make_layer(in_planes, dim, stride=1)
-  layer1 = BasicBlock(in_planes, dim, stride)
-  layer2 = BasicBlock(dim, dim, stride)
+function makeLayer(in_planes, dim, stride=1, x::AbstractArray)
+  layer1 = BasicBlock(in_planes, dim, stride, x)
+  layer2 = BasicBlock(dim, dim, stride, x)
   return Chain(
     layer1,
     layer2,
-  ),dim
+  )
 end
 
 # """ResNet+FPN, output resolution are 1/8 and 1/2.
 #     Each block has 2 layers.
 #     """
 
-function ResNetFPN_8_2(config, x)
+function ResNetFPN_8_2(x::AbstractArray)
   # Config
-  inital_dim = config["inital_dim"]
-  block_dims = config["block_dims"]
+  inital_dim = 128
   # Class Variable
   in_planes = inital_dim
+  inner_dim = 196
+  outer_dim = 256
   # Networks
   conv1 = Conv((7,7), 1 => inital_dim, stride=2, padding=3, bias=False)
   bn1 = BatchNorm(initial_dim,relu)
-  layer1,out_planes = _make_layer(in_planes, block_dims[0], stride=1) # 1/2
-  layer2,out1_planes = _make_layer(out_planes, block_dims[1], stride=2) # 1/4
-  layer3,_ = _make_layer(out1_planes, block_dims[2], stride=2) # 1/8
+  layer1 = makeLayer(in_planes, in_planes, stride=1 ,x) # 1/2
+  layer2 = makeLayer(in_planes, inner_dim, stride=2, x) # 1/4
+  layer3 = makeLayer(inner_dim, outer_dim , stride=2, x) # 1/8
    # 3. FPN upsample
    layer3_outconv = conv1x1(block_dims[2], block_dims[2])
    layer2_outconv = conv1x1(block_dims[1], block_dims[2])
@@ -77,21 +78,15 @@ function ResNetFPN_8_2(config, x)
    # FPN
    x3_out = layer3_outconv(x3)
    x2_out = layer2_outconv(x2)
+   x3_out_2x = interpolate(x3_out,BSpline(Linear()))
+   x2_out = layer2_outconv2(x2_out + x3_out_2x)
+   
+   x1_out = layer1_outconv(x1)
+   x2_out_2x = interpolate(x2_out,BSpline(Linear()))
+   x1_out = self.layer1_outconv2(x1_out + x2_out_2x)
 
-
-
-  #  # FPN
-  #       x3_out = self.layer3_outconv(x3)
-
-  #       x2_out = self.layer2_outconv(x2)
-  #       x3_out_2x = F.interpolate(x3_out, size=(x2_out.shape[2:]), mode='bilinear', align_corners=True)
-  #       x2_out = self.layer2_outconv2(x2_out + x3_out_2x)
-
-  #       x1_out = self.layer1_outconv(x1)
-  #       x2_out_2x = F.interpolate(x2_out, size=(x1_out.shape[2:]), mode='bilinear', align_corners=True)
-  #       x1_out = self.layer1_outconv2(x1_out + x2_out_2x)
-
-  #       return [x3_out, x1_out]
+   return [x3_out, x1_out]
+end
 
 
   
